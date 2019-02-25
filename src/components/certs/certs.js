97b984c08cd682bd110a8fb653f7b223014f012b
tommy3531 <thomas.marler@hotmail.com>
2019-02-25 16:47:01 -0600

    Commented out certs

--- src/components/certs/certs.js
+++ src/components/certs/certs.js
@@ -1,41 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import Helmet from 'react-helmet'
+// import { StaticQuery, graphql } from 'gatsby'
 
-function Certs({ description, lang, meta, keywords, title }) {
-  return (
-    <StaticQuery
-      query={detailsQuery}
-      render={data => {
-        return (
-            <h1></h1>
-        )
-      }}
-    />
-  )
-}
+// // function Certs({ description, lang, meta, keywords, title }) {
+// //   return (
+// //     <StaticQuery
+// //       query={detailsQuery}
+// //       render={data => {
+// //         return (
+// //             <h1></h1>
+// //         )
+// //       }}
+// //     />
+// //   )
+// // }
 
-export default Certs
+// export default Certs
 
-const detailsQuery = graphql`
-    query {
-    allMediumPost(sort: { fields: [createdAt], order: DESC }) {
-      edges {
-        node {
-          id
-          title
-          virtuals {
-            subtitle
-            previewImage {
-              imageId
-            }
-          }
-          author {
-            name
-          }
-        }
-      }
-    }
-  }
-`
+// const detailsQuery = graphql`
+//     query {
+//     allMediumPost(sort: { fields: [createdAt], order: DESC }) {
+//       edges {
+//         node {
+//           id
+//           title
+//           virtuals {
+//             subtitle
+//             previewImage {
+//               imageId
+//             }
+//           }
+//           author {
+//             name
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
